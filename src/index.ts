daeb9e14148f2c967b5da6e81023a3a65069261e
Siddha <siddha@Siddhas-MacBook-Pro.local>
2020-06-07 01:51:47 -0400

    working version

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,40 @@
-#!/usr/bin/env node
-import program from 'commander';
-import fs from 'fs';
-import { exec } from 'child_process';
-
 const START_IDENTIFIER = 'REACT_NAVIGATION_GENERATED_OUTPUT:';
 
-program.option('-o, --outputpath <path>', 'output path');
+const generateRouteMap = (
+  [routeName, route]: [string, any],
+  initRoute: string,
+  routeMap: any,
+) => {
+  const newRouteName =
+    initRoute === '' ? routeName : `${initRoute}.${routeName}`;
 
-program.parse(process.argv);
+  routeMap[routeName] = {
+    routeName: newRouteName,
+  };
 
-if (program.outputpath) {
-  // MARK requires that an ios simulator is running
-  const expoProcess = exec('expo start -i');
-
-  if (expoProcess && expoProcess.stdout) {
-    expoProcess.stdout.on('data', (data: any) => {
-      const output = data.toString();
-      if (output.includes(START_IDENTIFIER)) {
-        const routeMapJson = output.substring(
-          output.indexOf(START_IDENTIFIER) + START_IDENTIFIER.length,
-          output.lastIndexOf('}') + 1,
-        );
-        const outputPath = process.cwd() + program.outputpath;
-        const tsString = `const routes = ${routeMapJson};export default routes;`;
-        fs.writeFileSync(outputPath, tsString);
-        expoProcess.kill();
-        console.log('Route map created at ' + program.outputpath);
-      }
-    });
+  if (route.children) {
+    for (const child of Object.entries(route.children)) {
+      generateRouteMap(child, newRouteName, routeMap[routeName]);
+    }
   }
-} else {
-  console.log('No output path');
-}
+  return routeMap;
+};
 
-// const generateRouteMap = (
-//   [routeName, route]: [string, any],
-//   initRoute: string,
-//   routeMap: any,
-// ) => {
-//   const newRouteName =
-//     initRoute === '' ? routeName : `${initRoute}.${routeName}`;
-//   if (route.children) {
-//     routeMap[routeName] = {
-//       routeName: newRouteName,
-//     };
-//     for (const child of Object.entries(route.children)) {
-//       generateRouteMap(child, newRouteName, routeMap[routeName]);
-//     }
-//   } else {
-//     routeMap[routeName] = {
-//       routeName: newRouteName,
-//     };
-//   }
-//   return routeMap;
-// };
+const generateRootRoutes = (rootRouteMap: any) => {
+  let rootRoutes = {} as any;
+  for (const [routeName, route] of Object.entries(rootRouteMap)) {
+    rootRoutes = {
+      ...rootRoutes,
+      ...generateRouteMap([routeName, route], '', {}),
+    };
+  }
+  return rootRoutes;
+};
 
-// // MARK generates routeMap.json
-// const generateRootRoutes = (rootRouteMap: any) => {
-//   let rootRoutes = {} as any;
-//   for (const [routeName, route] of Object.entries(rootRouteMap)) {
-//     rootRoutes = {
-//       ...rootRoutes,
-//       ...generateRouteMap([routeName, route], '', {}),
-//     };
-//   }
-//   return rootRoutes;
-// };
+const logRootRoutes = (rootRouteMap: any) => {
+  console.log(
+    START_IDENTIFIER + JSON.stringify(generateRootRoutes(rootRouteMap)),
+  );
+};
 
-// const logRootRoutes = (rootRouteMap: any) => {
-//   console.log(
-//     START_IDENTIFIER + JSON.stringify(generateRootRoutes(rootRouteMap)),
-//   );
-// };
-
-// export { logRootRoutes };
+export { logRootRoutes };
